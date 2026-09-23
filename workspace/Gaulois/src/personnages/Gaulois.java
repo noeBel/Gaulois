ddfecd9b0d7a56a12e8ede3688f980a22dd757a5
Noe Bellon <bln5865b@ensfsi.onmicrosoft.com>
2026-09-23 14:45:22 +0200

    Premiers persos

---
+++ workspace/Gaulois/src/personnages/Gaulois.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion=1;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole()+"\""+texte+"\"");
+	}
+	private String prendreParole() {
+		return "Le Gaulois "+nom+" : ";
+	}
+}
